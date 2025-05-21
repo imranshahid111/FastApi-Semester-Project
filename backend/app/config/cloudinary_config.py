8dbe48a554c6c1e5194146ff1d81a19f95872d20
imran shahid <imranshahidkhan222@gmail.com>
2025-05-21 21:17:09 +0500

    fetch

--- backend/app/config/cloudinary_config.py
+++ backend/app/config/cloudinary_config.py
@@ -2,8 +2,8 @@ import cloudinary
 import cloudinary.uploader
 
 cloudinary.config(
-    cloud_name="dwazlkch3",
-    api_key="425778315372321",
-    api_secret="SveRy35zeQgV0qvvkitkX6Afp3s",
+    cloud_name="",
+    api_key="",
+    api_secret="",
     secure=True
 )
